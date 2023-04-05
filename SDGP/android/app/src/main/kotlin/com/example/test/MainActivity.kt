0dfa86693a757b2706ed4d25437d660561cf966b
Sadeepa Ranasinghe <124652227+sadeepa5544@users.noreply.github.com>
2023-04-05 15:07:30 +0530

    change the code

--- SDGP/android/app/src/main/kotlin/com/example/test/MainActivity.kt
+++ SDGP/android/app/src/main/kotlin/com/example/test/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.test
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity;
 
-class MainActivity: FlutterActivity() {
+public class MainActivity extends FlutterFragmentActivity{
+
 }
