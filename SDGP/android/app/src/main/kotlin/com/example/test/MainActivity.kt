64d0371707c1ba119af703485aa617fe20a9c5f9
Shehan Amantha <113180126+ShehanAmantha1@users.noreply.github.com>
2023-04-05 17:16:39 +0530

    makin errors

--- SDGP/android/app/src/main/kotlin/com/example/test/MainActivity.kt
+++ SDGP/android/app/src/main/kotlin/com/example/test/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.test
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity;
 
-class MainActivity: FlutterActivity() {
+public class MainActivity extends  FlutterActivity() {
 }
