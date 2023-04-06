60d9a1e7a0e332bfd24324b0460d15f0ad50f866
Sadeepa Ranasinghe <124652227+sadeepa5544@users.noreply.github.com>
2023-04-06 09:27:51 +0530

    Revert "Commit"
    This reverts commit dd209f42c21f7dff21e139ef58a7d4645b99bd78.

--- SDGP/lib/main.dart
+++ SDGP/lib/main.dart
@@ -18,65 +18,16 @@ import 'package:myapp/pages/loading-page.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Skin Diseases Analyzer',
-        debugShowCheckedModeBanner: false,
-        scrollBehavior: MyCustomScrollBehavior(),
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const Loadingpage());
-  }
-}
-
-class LoginOption extends StatelessWidget {
-  const LoginOption({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        const Text(
-          "Existing user?",
-          style: TextStyle(
-            fontSize: 16,
-          ),
-        ),
-        const SizedBox(
-          height: 16,
-        ),
-        Container(
-          height: 40,
-          decoration: BoxDecoration(
-            color: const Color(0xFF1C1C1C),
-            borderRadius: const BorderRadius.all(
-              Radius.circular(25),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: const Color(0xFF1C1C1C).withOpacity(0.2),
-                spreadRadius: 3,
-                blurRadius: 4,
-                offset: const Offset(0, 3),
-              ),
-            ],
-          ),
-          child: const Center(
-            child: Text(
-              "LOGIN",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
+	@override
+	Widget build(BuildContext context) {
+	return MaterialApp(
+		title: 'Skin Diseases Analyzer',
+		debugShowCheckedModeBanner: false,
+		scrollBehavior: MyCustomScrollBehavior(),
+		theme: ThemeData(
+		primarySwatch: Colors.blue,
+		),
+    home:const Loadingpage()
+	);
+	}
 }
