4f2aaa9974d9a43f055e91cb90320fd569bfce81
anupa47 <124699623+anupa47@users.noreply.github.com>
2023-04-06 08:18:12 +0530

    Changed the upload medical history page

--- SDGP/lib/pages/upload-medical-hisrtory-page.dart
+++ SDGP/lib/pages/upload-medical-hisrtory-page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter/rendering.dart';
 import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/pages/medical-history-page.dart';
 import 'package:myapp/utils.dart';
-
+import 'package:flutter/material.dart';
 class UploadmedicalHistorypage extends StatelessWidget {
   @override
 //   Widget build(BuildContext context) {
@@ -239,47 +240,167 @@ class UploadmedicalHistorypage extends StatelessWidget {
 //       ),
 //           );
 //   }
+//    Widget build(BuildContext context) {
+//      const appTitle = 'Add your details';
+// return MaterialApp(
+//       title: appTitle,
+//        home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text(appTitle),
+//         ),
+//        body: const MyCustomForm(),
+//        ),
+//     );
+//    }
+// }
+//  class MyCustomForm extends StatelessWidget {
+//    const MyCustomForm({super.key});
+//
+//    @override
+//   Widget build(BuildContext context) {
+//      return Column(
+//          crossAxisAlignment: CrossAxisAlignment.start,
+//          children: <Widget>[
+//      //const Padding(
+//      //padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+//     //child: TextField(
+//     //decoration: InputDecoration(
+//     //border: OutlineInputBorder(),
+//     //hintText: 'Enter your details',
+//     //),
+//     //),
+//     //),
+//     Padding(
+//     padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+//     child: TextFormField(
+//       decoration: const InputDecoration(
+//         //border: UnderlineInputBorder(),
+//         labelText: 'Enter your details',
+//       ),
+//     ),
+//
+//
+//
+//     ),
+//        ],
+//     );
+//
+//   }
+// }
+//
   Widget build(BuildContext context) {
-    const appTitle = 'Form Styling Demo';
+    final appTitle = 'Enter Your Details';
     return MaterialApp(
       title: appTitle,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text(appTitle),
+          title: Text(appTitle),
         ),
-        body: const MyCustomForm(),
+        body: MyCustomForm(),
       ),
     );
   }
 }
-class MyCustomForm extends StatelessWidget {
-  const MyCustomForm({super.key});
+// Create a Form widget.
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+
+
+// Defining a custom Form widget.
+class CustomForm extends StatefulWidget {
+  const CustomForm({super.key});
+
+
+  CustomFormState createState() {
+    return CustomFormState();
+  }
+}
+
+// Defining a corresponding State class.
+class CustomFormState extends State<CustomForm> {
+
+  final _formKey = GlobalKey<FormState>();
+
+
+  Widget build(BuildContext context) {
+    // Building a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          // Add TextFormFields and ElevatedButton here.
+        ],
+      ),
+    );
+  }
+  // ElevatedButton(
+  // onPressed: () {
+  //
+  // // Validate returns true if the form is valid, or false otherwise.
+  // if (_formKey.currentState!.validate()) {
+  //
+  // // If the form is valid, display a snackbar. In the real world,
+  // // you'd often call a server or save the information in a database.
+  // ScaffoldMessenger.of(context).showSnackBar(
+  // const SnackBar(content: Text('Processing Data')),
+  // );
+  // }
+  // },
+  // child: const Text('Submit'),
+  // ),
+}
+
+// Create a corresponding State class. This class holds data related to the form.
+  class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-    const Padding(
-    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-    child: TextField(
-    decoration: InputDecoration(
-    border: OutlineInputBorder(),
-    hintText: 'Enter a search term',
-    ),
-    ),
-    ),
-    Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-    child: TextFormField(
-      decoration: const InputDecoration(
-        border: UnderlineInputBorder(),
-        labelText: 'Enter your username',
-      ),
-    ),
-    ),
-        ],
-    );
+  // Build a Form widget using the _formKey created above.
+  return Form(
+  key: _formKey,
+  child: Column(
+  crossAxisAlignment: CrossAxisAlignment.start,
+  children: <Widget>[
+  TextFormField(
+  decoration: const InputDecoration(
+  icon: const Icon(Icons.person),
+  hintText: 'Enter your name',
+  labelText: 'Name',
+  ),
+  ),
+  TextFormField(
+  decoration: const InputDecoration(
+  icon: const Icon(Icons.phone),
+  hintText: 'Enter a phone number',
+  labelText: 'Phone',
+  ),
+  ),
+  TextFormField(
+  decoration: const InputDecoration(
+  icon: const Icon(Icons.calendar_today),
+  hintText: 'Enter you email',
+  labelText: 'email',
+  ),
+  ),
+  TextFormField(
+  decoration: const InputDecoration(
+  icon: const Icon(Icons.calendar_today),
+  hintText: 'Enter your date of birth',
+  labelText: 'Dob',
+  ),
+  ),
+
+  ],
+  ),
+  );
   }
-}
+  }
 
