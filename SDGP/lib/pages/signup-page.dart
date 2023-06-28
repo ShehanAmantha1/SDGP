ea49637795884b1030194298ae7206243c1876f9
seniya23 <124796641+seniya23@users.noreply.github.com>
2023-06-28 17:24:09 +0530

    Update signup-page.dart

--- SDGP/lib/pages/signup-page.dart
+++ SDGP/lib/pages/signup-page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:myapp/pages/home-screen.dart';
 import 'package:myapp/pages/login-page.dart';
@@ -8,16 +7,16 @@ class Signuppage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          body: Container(
-            margin: EdgeInsets.all(24),
-            child:
+      body: Container(
+        margin: EdgeInsets.all(24),
+        child:
             Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              _header(context),
-              _inputFields(context),
-              _loginInfo(context),
-            ]),
-          ),
-        ));
+          _header(context),
+          _inputFields(context),
+          _loginInfo(context),
+        ]),
+      ),
+    ));
   }
 
   _header(context) {
@@ -51,4 +50,79 @@ class Signuppage extends StatelessWidget {
           height: 10,
         ),
         TextField(
- 
+          decoration: InputDecoration(
+            hintText: "Email",
+            fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
+            filled: true,
+            prefixIcon: Icon(Icons.email_outlined),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide.none),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Password",
+            fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
+            filled: true,
+            prefixIcon: Icon(Icons.password_outlined),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide.none),
+          ),
+          obscureText: true,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Confirmed Password",
+            fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
+            filled: true,
+            prefixIcon: Icon(Icons.password_outlined),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide.none),
+          ),
+          obscureText: true,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (_) => HomeScreen()));
+          },
+          child: Text(
+            "Sign Up",
+            style: TextStyle(fontSize: 20),
+          ),
+          style: ElevatedButton.styleFrom(
+            shape: StadiumBorder(),
+            padding: EdgeInsets.symmetric(vertical: 16),
+          ),
+        )
+      ],
+    );
+  }
+
+  _loginInfo(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("Already have an account?"),
+        TextButton(
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => LoginPage()));
+            },
+            child: Text("Login"))
+      ],
+    );
+  }
+}
